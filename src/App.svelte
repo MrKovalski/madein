1d1e71e10bf1e15e15846e4f115caad53d094d9a
Filip Ilic <filip@filipilic.com>
2021-01-14 16:49:46 +0100

    pocetni komit

---
+++ src/App.svelte
@@ -0,0 +1,78 @@
+<script>
+	import Quagga from 'quagga';
+
+	import cbe from './static/data/countrybyean.json';
+	let prvaTri = "";
+	let barcode = "";
+	let zemljaText = "Neutvrđeno";
+
+	function prepoznajZemlju(){
+		let zo = cbe.map(el => el.barcode);
+		let zoi = zo.indexOf(barcode)
+		
+		if(zoi !== NaN){
+			zemljaText = cbe[zoi].country;
+		} else{
+			zemljaText = "Neutvrđeno";
+		}
+	}
+</script>
+
+<main>
+	<h1>Odakle potiče proizvod?</h1>
+	<h3>Unesi prva tri broja barkoda</h3>
+	<div id="bc-container">
+		<form action="javascript:void(0);">
+			<input on:keyup={prepoznajZemlju} id="input-barkod" type="number" placeholder="Barkod..." bind:value={barcode}>
+		</form>
+		<p id="zemlja-text">{zemljaText}</p>
+	</div>
+</main>
+
+<style>
+	
+main{
+	margin: 0 auto;
+	width: 100vw;
+}
+h1 {
+	text-transform: uppercase;
+	font-size: 2em;
+	text-align: center;
+}
+h3 {
+	margin-top: 2em;
+	text-transform: uppercase;
+	font-size: 1.2em;
+	text-align: center;
+}
+#bc-container{
+	margin: 0 auto;
+	display: grid;
+	grid-template-columns: 1fr;
+	justify-items: center;
+}
+#input-barkod{
+	border-radius: 0.5em;
+	border: 0.2em solid black;
+	
+}
+#zemlja-text{
+	background-color: rgb(160, 235, 160);
+	padding: 0.5em 1em;
+	font-size: 3em;
+	width: 50%;
+	margin-bottom: 1em;
+	text-align: center;
+	text-transform: uppercase;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+input[type=number] {
+    -moz-appearance: textfield;
+}
+</style>
